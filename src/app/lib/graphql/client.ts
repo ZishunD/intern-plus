9cdff11f6bee4232a2a782c50dba1ccfd95e310d
ZishunD <gaozishun.david@gmail.com>
2025-09-20 14:31:33 +0700

    program, applyForm, success page

--- src/app/lib/graphql/client.ts
+++ src/app/lib/graphql/client.ts
@@ -1,4 +1,4 @@
 import { GraphQLClient } from "graphql-request";
 
-export const ENDPOINT = `${process.env.NEXT_PUBLIC_API_URL}/graphql`; // 或部署地址
+export const ENDPOINT = `${process.env.NEXT_PUBLIC_GRAPHQL_LINK}/graphql`; // 或部署地址
 export const client = new GraphQLClient(ENDPOINT);
