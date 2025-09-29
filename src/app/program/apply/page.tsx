a63276c40fc406d6b7eb6994468b3aae9305d4f8
ZishunD <gaozishun.david@gmail.com>
2025-09-29 08:38:30 +0700

    applictaion page updation

--- src/app/program/apply/page.tsx
+++ src/app/program/apply/page.tsx
@@ -1,4 +1,5 @@
-"use client";
+export const dynamic = "force-dynamic";
+("use client");
 
 import ApplyProgramForm from "@/components/apply/ApplyProgramForm";
 import { useState, useEffect } from "react";
